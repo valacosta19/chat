e978ef1538e023aa28ed1878d3f8e65010b7f5da
Valentina Acosta <valen.acosta19@hotmail.com>
2022-09-06 09:11:50 -0300

    initializing server

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const {createServer} = require('http')
+const realTimeServer = require('./realtimeServer')
+const path = require('path')
+
+const app = express()
+const httpServer = createServer(app)
+
+//Settings
+app.set('port', process.env.PORT || 3000)
+app.set("views", path.join(__dirname, "views"))
+
+//Routes
+app.use(require("./routes"));
+
+//Public
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Start server
+httpServer.listen(app.get('port'),() => {
+  console.log(`el servidor esta corriendo en el puerto ${app.get('port')}`)
+})
+
+//Calling socket server
+realTimeServer(httpServer)
